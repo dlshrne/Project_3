78bfec41a175aec7a743e8df702327c6f4bdef62
dlshrne <dlshrne@gmail.com>
2019-08-14 18:09:26 +0900

    2019.08.14 html / css 수정 v 1.2

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -1,6 +1,25 @@
 $(function(){
 
-    $('.main_banner_slides').bxSlider();
+    $('.main_banner_slides').bxSlider(); // 배너슬라이드
+
+
+    var swiper = new Swiper('.main_nav', {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+
+    
+
+
+
+
+
+
+
 
 
 
